ac8bf891811af38b9d17df326750cd884c25c594
lecielpourlamer <shlim20190902@gmail.com>
2023-10-17 13:20:18 +0900

    자바 ORM 표준 프로그래밍 인프런 강좌 섹션0~2완료

---
+++ src/main/java/hellojpa/JapMain.java
@@ -0,0 +1,54 @@
+package hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class JapMain {
+
+    public static void main(String[] args)
+    {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("Donald");
+//
+//            em.persist(member);
+
+            //Member findMember = em.find(Member.class, 1L);
+            //findMember.setName("HelloJPA");
+
+            //em.remove(findMember);
+            //System.out.println("findMember.id = " + findMember.getId());
+            //System.out.println("findMember.name = " + findMember.getName());
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
